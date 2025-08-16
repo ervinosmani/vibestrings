2df71ad0d7fa20bf12cfd616cae616d1e585993b
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2025-08-16 21:33:28 +0200

    add Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    container: { center: true, padding: "1rem" },
+    extend: {},
+  },
+  plugins: [],
+};
